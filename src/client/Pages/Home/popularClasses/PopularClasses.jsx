08a5b0fea458ea37c05c8d8385f78427d5554640
dev-abulhassan <abulhassan.dev@gmail.com>
2023-06-09 23:18:42 +0600

    popular class responsive

--- src/client/Pages/Home/popularClasses/PopularClasses.jsx
+++ src/client/Pages/Home/popularClasses/PopularClasses.jsx
@@ -22,16 +22,16 @@ const PopularClasses = () => {
   return (
     <div className="bg-[#050101] text-white py-10">
       <Container>
-        <div className="flex items-end justify-between">
+        <div className="md:flex items-end justify-between">
           <div className="md:text-left">
-            <div className="py-3 text-[#E6382E] text-xl ">
+            <div className="py-3 text-[#E6382E] md:text-xl ">
               <strong>OUR CLASS</strong>
             </div>
             <div className="pb-5">
-              <h2 className="text-4xl font-bold">CHOOSE YOUR DANCE STYLE</h2>
+              <h2 className=" font-bold md:text-4xl text-2xl md:font-bold ">CHOOSE YOUR DANCE STYLE</h2>
             </div>
-            <div className="pt-5">
-              <p className="text-gray-600 font-medium text-lg ">
+            <div className="pt-5 pb-4">
+              <p className="text-gray-500  text-lg font-light md:font-normal">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
                 tellus, luctus nec <br /> ullamcorper mattis, pulvinar dapibus
                 leo.
@@ -39,13 +39,13 @@ const PopularClasses = () => {
             </div>
           </div>
           <Link to={"/about"}>
-            <button className="py-4 px-8 rounded-md uppercase bg-[#E6382E] transition-all hover:bg-rose-500 font-semibold text-white">
+            <button className="py-4 px-8 md:w-48  w-full rounded-md uppercase bg-[#E6382E] transition-all hover:bg-rose-500 font-semibold text-white">
              View All Class
             </button>
           </Link>
         </div>
 
-        <div className="grid grid-cols-3 gap-8 py-10">
+        <div className="md:grid grid-cols-3 gap-8 py-10">
           {courses.slice(0, 6).map((item) => (
             <ClassCard key={item.id} item={item}></ClassCard>
           ))}
