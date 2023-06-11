d417dd655f69b0cd785e92e4c9c933b2e538d881
dev-abulhassan <abulhassan.dev@gmail.com>
2023-06-11 21:16:28 +0600

    login from

--- src/client/Pages/Login/Login.jsx
+++ src/client/Pages/Login/Login.jsx
@@ -2,16 +2,18 @@ import Container from "../../../components/Container";
 import LoginImg from "../../../assets/Login/login.jpg";
 import { useForm } from "react-hook-form";
 
-import { Link, Navigate, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../../provaider/AuthProvider";
 import { useState } from "react";
 
 const Login = () => {
-  const [err, setErr]= useState('')
-  const { signIn } = useContext(AuthContext);
+  const [err, setErr] = useState("");
+  const { signIn, loading, setLoading, signInWithGoogle, resetPassword } =
+    useContext(AuthContext);
+  const navigate = useNavigate();
   const location = useLocation();
-  console.log("login page location", location);
+
   const from = location.state?.from?.pathname || "/";
   const {
     register,
@@ -24,23 +26,38 @@ const Login = () => {
     signIn(data.email, data.password)
       .then((result) => {
         const loggedUser = result.user;
-       
-        Navigate(from, { replace: true });
+
+        navigate(from, { replace: true });
       })
       .catch((error) => {
         console.log(error);
-        setErr(error.message)
-
+        setLoading(false);
+        setErr(error.message);
       });
-     
+
     console.log(data);
+  };
+
+  const handleGoogleSignIn = () => {
+    signInWithGoogle()
+      .then((result) => {
+        console.log(result.user);
+
+        saveUser(result.user);
+        navigate(from, { replace: true });
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err.message);
+        toast.error(err.message);
+      });
   };
 
   return (
     <div className="py-24">
       <Container>
-        <div className="flex ">
-          <div>
+        <div className="md:flex items-center  gap-60">
+          {/* <div>
             <img
               className="rounded-lg"
               src={LoginImg}
@@ -48,32 +65,36 @@ const Login = () => {
               height="300"
               width="500"
             />
-          </div>
-          <div className=" border-2 w-1/2">
+          </div> */}
+          <div className=" border-2 mx-auto w-1/3">
             <h3>Login From Here</h3>
 
-            <div >
+            <div>
               <div className=" p-8 ">
                 <form onSubmit={handleSubmit(onsubmit)}>
                   <div className="text-rose-800">{err}</div>
                   <div className="mt-4 flex items-center justify-between">
                     <span className="border-b w-1/5 lg:w-1/4" />
-                    <a
-                      href="#"
-                      className="text-xs text-center text-gray-500 uppercase"
-                    >
+                    <p className="text-xs text-center text-gray-500 uppercase">
                       or login with email
-                    </a>
+                    </p>
                     <span className="border-b w-1/5 lg:w-1/4" />
                   </div>
-
+                  <div className="mt-8">
+                    <button
+                      onClick={handleGoogleSignIn}
+                      className="bg-rose-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-rose-600"
+                    >
+                      Google
+                    </button>
+                  </div>
                   <div className="mt-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2">
                       Email Address
                     </label>
                     <input
                       {...register("email")}
-                      className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+                      className="bg-rose-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
                       type="email"
                     />
                   </div>
@@ -87,20 +108,24 @@ const Login = () => {
                       </a>
                     </div>
                     <input
-                      className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+                      className="bg-rose-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
                       type="password"
                       {...register("password")}
                     />
                   </div>
 
                   <div className="mt-8">
-                    <button className="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">
+                    <button className="bg-rose-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-rose-600">
                       Login
                     </button>
                   </div>
+
                   <div className="mt-4 flex items-center justify-between">
                     <span className="border-b w-1/5 md:w-1/4" />
-                    <Link to={'/signup'} className="text-base text-gray-500 uppercase">
+                    <Link
+                      to={"/signup"}
+                      className="text-base text-white border-2 px-2 py-2 rounded-md bg-rose-500 hover:bg-rose-600 uppercase"
+                    >
                       or sign up
                     </Link>
                     <span className="border-b w-1/5 md:w-1/4" />
--- src/client/Pages/SignUp/SignUp.jsx
+++ src/client/Pages/SignUp/SignUp.jsx
@@ -4,12 +4,13 @@ import { useForm } from "react-hook-form";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../../provaider/AuthProvider";
-import toast, { Toaster } from 'react-hot-toast';
-
+import toast, { Toaster } from "react-hot-toast";
 
 const SignUp = () => {
-  const { createUser } = useContext(AuthContext);
-  const navigate = useNavigate()
+  const { createUser, loading, setLoading, signInWithGoogle } =
+    useContext(AuthContext);
+  const navigate = useNavigate();
+  const from = location.state?.from?.pathname || "/";
   const {
     register,
     handleSubmit,
@@ -19,23 +20,39 @@ const SignUp = () => {
 
   const onsubmit = (data) => {
     if (data.password !== data.confirm_password) {
-      toast.error("Password didn't Match.")
+      toast.error("Password didn't Match.");
       return;
     }
     createUser(data.email, data.password)
       .then((result) => {
-         navigate("/");
+        navigate(from, { replace: true });
         console.log(createUser);
       })
       .catch((error) => console.log(error));
+
     console.log(data);
   };
 
+  // Google signin
+  const handleGoogleSignIn = () => {
+    signInWithGoogle()
+      .then((result) => {
+        console.log(result.user);
+
+        navigate(from, { replace: true });
+      })
+      .catch((err) => {
+        setLoading(false);
+
+        toast.error(err.message);
+      });
+  };
+
   return (
-    <div>
+    <div className="py-12">
       <Container>
-        <div className="flex ">
-          <div>
+        <div className="md:flex gap-32 ">
+          {/* <div>
             <img
               className="rounded-lg"
               src={LoginImg}
@@ -43,31 +60,38 @@ const SignUp = () => {
               height="300"
               width="500"
             />
-          </div>
-          <div className=" border-2 w-1/2">
-            <h3>Login From Here</h3>
+          </div> */}
+          <div className=" border-2 w-1/3 mx-auto">
+            <h3 className="text-rose-500 font-semibold text-xl pt-4">Login From Here</h3>
 
             <div>
-              <div className=" p-8 ">
+              <div className=" px-8 ">
                 <form onSubmit={handleSubmit(onsubmit)}>
+                
+                  <div className="mt-8">
+                    <button
+                      onClick={handleGoogleSignIn}
+                      className="bg-gradient-to-r from-[#1e1b4b] via-purple-800 to-rose-600 text-white font-bold py-2 px-4 w-full rounded hover:from-pink-500 hover:to-yellow-500 "
+                    >
+                      Google
+                    </button>
+                  </div>
+
                   <div className="mt-4 flex items-center justify-between">
                     <span className="border-b w-1/5 lg:w-1/4" />
-                    <a
-                      href="#"
-                      className="text-xs text-center text-gray-500 uppercase"
-                    >
+                    <p className="text-xs text-center text-rose-500 uppercase">
                       or login with email
-                    </a>
+                    </p>
                     <span className="border-b w-1/5 lg:w-1/4" />
                   </div>
 
                   <div className="mt-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2">
+                    <label className="block text-rose-500 text-sm font-bold mb-2">
                       Email Address
                     </label>
                     <input
                       {...register("email")}
-                      className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+                      className="bg-rose-200 text-rose-700 focus:outline-none focus:shadow-outline border border-rose-300 rounded py-2 px-4 block w-full appearance-none"
                       type="email"
                       placeholder="Email"
                     />
@@ -75,12 +99,12 @@ const SignUp = () => {
 
                   <div className="mt-4">
                     <div className="flex justify-between">
-                      <label className="block text-gray-700 text-sm font-bold mb-2">
+                      <label className="block text-rose-700 text-sm font-bold mb-2">
                         Password
                       </label>
                     </div>
                     <input
-                      className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+                      className="bg-rose-200 text-rose-700 focus:outline-none focus:shadow-outline border border-rose-300 rounded py-2 px-4 block w-full appearance-none"
                       type="password"
                       {...register("password")}
                     />
@@ -88,21 +112,21 @@ const SignUp = () => {
 
                   <div className="mt-4">
                     <div className="flex justify-between">
-                      <label className="block text-gray-700 text-sm font-bold mb-2">
+                      <label className="block text-rose-700 text-sm font-bold mb-2">
                         Confirm Password
                       </label>
-                      <a href="#" className="text-xs text-gray-500">
+                      <a href="#" className="text-xs text-rose-500">
                         Forget Password?
                       </a>
                     </div>
                     <input
-                      className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+                      className="bg-rose-200 text-rose-700 focus:outline-none focus:shadow-outline border border-rose-300 rounded py-2 px-4 block w-full appearance-none"
                       type="password"
                       {...register("confirm_password")}
                     />
                   </div>
 
-                  <div className="mt-4 py-2 px-2 text-left bg-gray-500 rounded text-white">
+                  <div className="mt-4 py-2 px-2 text-left bg-rose-500 rounded text-white">
                     <label htmlFor="image" className="block mb-2 text-sm">
                       Select Image:
                     </label>
@@ -115,14 +139,15 @@ const SignUp = () => {
                   </div>
 
                   <div className="mt-8">
-                    <button className="bg-gray-700  text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">
+                    <button className="bg-gradient-to-r from-[#1e1b4b] via-purple-800 to-rose-600  text-white font-bold py-2 px-4 w-full rounded hover:from-pink-500 hover:to-yellow-500 transition-all">
                       Sign Up
                     </button>
                   </div>
+
                   <div className="mt-4 flex items-center justify-between">
                     <span className="border-b w-1/5 md:w-1/4" />
-                    <Link className="text-xs text-gray-500 uppercase">
-                      or sign up
+                    <Link to={'/sign-in'} className="text-base text-white border-2 px-2 py-2 rounded-md bg-rose-500 hover:bg-rose-600 uppercase">
+                      or sign In
                     </Link>
                     <span className="border-b w-1/5 md:w-1/4" />
                   </div>
